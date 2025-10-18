1098b2d0986ccc9f0914c3af191328af5c6f8bd4
Imtious Tanvir <82751228+imtious-tanvir@users.noreply.github.com>
2025-10-18 19:49:36 +0600

    Add function to sum two lists in reverse direction
    Implement a function to add two lists in reverse order and return the result as a list.

---
+++ add_two_list_in_reverse_direction.py
@@ -0,0 +1,22 @@
+def add(list_1, list_2):
+    update_list_1 = list_1[::-1]
+    update_list_2 = list_2[::-1]
+
+    first_number = ''
+    second_number = ''
+
+    for number in update_list_1:
+        first_number += str(number)
+    for number in update_list_2:
+        second_number += str(number)
+
+    result = int(first_number) + int(second_number)
+    final_list = [int(d) for d in str(result)][::-1]
+
+    return final_list
+
+
+l1 = [2,4,3]
+l2 = [5,6,4]
+
+print(add(l1, l2))
