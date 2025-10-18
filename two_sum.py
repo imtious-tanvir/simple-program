264b3f0ce14a99f6fc58e038078fc8d704495dd2
Imtious Tanvir <82751228+imtious-tanvir@users.noreply.github.com>
2025-10-18 18:47:44 +0600

    Refactor two_sum method for parameter consistency

--- two_sum.py
+++ two_sum.py
@@ -1,15 +1,16 @@
 class Solution:
+    def two_sum(self, number_list, target):
 
-  def two_sum(self, target, numbers_list):
-    seen_values = {}
-    for index, number in enumerate(numbers_list):
-      compare = target - number
-      if compare in seen_values:
-        return [seen_values[compare], index]
-        seen_values[number] = index
-    raise ValueError("No two sum solution.")
+        seen_values = {}
+        for index, number in enumerate(number_list):
+            compare = target - number
+            if compare in seen_values:
+                return [seen_values[compare], index]
+            seen_values[number] = index
+        raise ValueError("No two sum solution")
+
 
 if __name__ == "__main__":
     nums = [2, 7, 11, 15]
     target = 9
-    print(Solution().twoSum(target, nums))
+    print(Solution().two_sum(nums, target))
