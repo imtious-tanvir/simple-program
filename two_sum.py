3857e4fe224c669510786f380de84398fb4bef89
Imtious Tanvir <82751228+imtious-tanvir@users.noreply.github.com>
2025-10-18 19:12:57 +0600

    Refactor two_sum function and update test case

--- two_sum.py
+++ two_sum.py
@@ -1,16 +1,13 @@
-class Solution:
-    def two_sum(self, number_list, target):
-
-        seen_values = {}
-        for index, number in enumerate(number_list):
-            compare = target - number
-            if compare in seen_values:
-                return [seen_values[compare], index]
-            seen_values[number] = index
-        raise ValueError("No two sum solution")
+def two_sum(number_list, target):
+    seen_values = {}
+    for index, number in enumerate(number_list):
+        compare = target - number
+        if compare in seen_values:
+            return [seen_values[compare], index]
+        seen_values[number] = index
+    raise ValueError("No two sum solution")
 
 
-if __name__ == "__main__":
-    nums = [2, 7, 11, 15]
-    target = 9
-    print(Solution().two_sum(nums, target))
+nums = [3, 2, 4]
+target = 6
+print(two_sum(nums, target))
